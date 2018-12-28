9c94be1b7d272efb2d7aae80ac6b9d26eb97e4d1
rachana velidandla <rachanavelidandla@MacBook-Pro-3.local>
2018-12-28 15:49:00 +0530

    first commit

---
+++ arithmetic_test.py
@@ -0,0 +1,8 @@
+import basic_arithmetic as ba
+
+print('the sum is ',ba.add(5,4))
+print('the difference is ',ba.sub(5,4))
+print('the  product is ',ba.mul(5,4))
+print('the quotient is ',ba.quo(5,4))
+print('the remainder is ',ba.rem(5,4))
+print('the average is ',ba.av(5,4))
---
+++ basic_arithmetic.py
@@ -0,0 +1,17 @@
+def add(x,y):
+	return x + y
+
+def sub(x,y):
+	return x - y
+
+def mul(x,y):
+	return x * y
+
+def quo(x,y):
+	return x/y
+
+def rem(x,y):
+	return x%y
+
+def av(x,y):
+	return (x + y)/2
